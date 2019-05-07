80dacee408e1c85c8c84a1e18f4c59c6b0956a26
unknown <1136908271@qq.com>
2019-05-07 14:53:52 +0800

    wq jjjjjjjjsssssqqqs# Changes to be committed:

--- GitTest/GitTest/GitTest.cpp
+++ GitTest/GitTest/GitTest.cpp
@@ -9,8 +9,7 @@ int main()
 	std::cout << "GitTest!\n"<<std::endl;
 	std::cout << "GithhhhTest!\n" << std::endl;
 	std::cout << "Hello World1122211!\n" << std::endl;
-	std::cout << "123456789!\n" << std::endl;
-    std::cout << "Hello World!\n"<<std::endl; 
+
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
